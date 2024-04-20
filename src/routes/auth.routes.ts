6acfd32e7e6d0b25d8fca59c44f0d7d49fe65586
benny <benpakmnz@gmail.com>
2024-04-20 18:18:28 +0300

    protecting auto login route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { login, signup, autoLogin } from "../controllers/auth.controller";
+import { isLogedin } from "../middlewares/auth-check";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
-
-router.post("/autologin", autoLogin);
+router.use(isLogedin);
+router.get("/autologin", autoLogin);
 
 export { router as authRouter };
